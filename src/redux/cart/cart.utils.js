9bf882147c434d9165a0f7adae034b130b80f1ed
kaollie74 <kaollie74@gmail.com>
2020-11-27 19:34:12 -0600

    added logic to cart drop down to display items in cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,7 +1,6 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find((el) => el.id === cartItemToAdd.id);
   if (existingCartItem) {
-    console.log("inside if");
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
